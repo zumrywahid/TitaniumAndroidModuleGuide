260095a2e145835578744db0b1e8c2b25ce6d440
zumrywahid <zumrywahid@gmail.com>
2013-06-30 21:59:01 +0530

    test

---
+++ example/app.js
@@ -0,0 +1,5 @@
+// Ensure that the module development guide module is loaded		
+var DevGuide = require('com.zumry.module');
+
+// Open the main category selection page
+require('navigator').start(DevGuide);
